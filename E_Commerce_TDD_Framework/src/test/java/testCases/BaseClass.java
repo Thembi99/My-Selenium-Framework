e84728be0bf0cd2bc0625880ffb4d21c8ca4a987
Thembi Mahwayi <thembi.mahwayi@globetom.com>
2025-10-15 14:03:59 +0200

    Added config properties
    Adding config properties file

--- E_Commerce_TDD_Framework/src/test/java/testCases/BaseClass.java
+++ E_Commerce_TDD_Framework/src/test/java/testCases/BaseClass.java
@@ -1,9 +1,13 @@
 package testCases;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import utilities.ReadConfig;
 
 public class BaseClass {
 
+    ReadConfig readConfig=new ReadConfig();
+    public static WebDriver driver;
     public void setUp(String browser)
     {
         if (browser.equals("chrome"))
--- E_Commerce_TDD_Framework/src/test/java/utilities/ReadConfig.java
+++ E_Commerce_TDD_Framework/src/test/java/utilities/ReadConfig.java
@@ -1,6 +1,61 @@
 package utilities;
 
-public class ReadConfig
-{
-    
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Properties;
+
+public class ReadConfig {
+
+    Properties properties;
+
+    //Function : Reads all properties from the config.properties File
+    public ReadConfig() {
+
+        File src = new File("./Configuration/config.properties");
+
+        try {
+            FileInputStream fis = new FileInputStream(src);
+            properties = new Properties();
+            properties.load(fis);
+
+        } catch (Exception e) {
+            System.out.println("Exception is " + e.getMessage());
+        }
+    }
+
+    //Function : Gets applications (URL)
+    public String getApplicationURL() {
+        String url = properties.getProperty("baseURL");
+        return url;
+    }
+
+    //Function : Gets Chrome Path
+    public String getChromePath() {
+        String chromepath = properties.getProperty("chromepath");
+        return chromepath;
+    }
+
+    //Function : Gets FireFox Path
+    public String getGeckoPath() {
+        String geckopath = properties.getProperty("geckopath");
+        return geckopath;
+    }
+
+    //Function : Gets MsEdge Path
+    public String getMsEdgePath() {
+        String msedgepath = properties.getProperty("msedgepath");
+        return msedgepath;
+    }
+
+    public String getUsername() {
+        String username = properties.getProperty("username");
+        return username;
+    }
+
+    public String getPassword() {
+        String password = properties.getProperty("password");
+        return password;   }
+
+
 }
+
