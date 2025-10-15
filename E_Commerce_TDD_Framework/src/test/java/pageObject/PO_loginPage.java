3f39957ee111462df4f52623e9f05647da64793f
Thembi Mahwayi <thembi.mahwayi@globetom.com>
2025-10-15 14:29:06 +0200

    Page object update
    Updating the page login PO

--- E_Commerce_TDD_Framework/src/test/java/pageObject/PO_loginPage.java
+++ E_Commerce_TDD_Framework/src/test/java/pageObject/PO_loginPage.java
@@ -1,9 +1,46 @@
 package pageObject;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.CacheLookup;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import testCases.BaseClass;
 
-public class PO_loginPage extends BaseClass
+public class PO_loginPage
 {
     WebDriver ldriver;
+
+    public PO_loginPage(WebDriver rdriver)
+    {
+        ldriver = rdriver;
+        PageFactory.initElements(rdriver,this);
+    }
+
+    @FindBy(xpath = "/html/body/div/div/div[2]/div[1]/div/div/form/div[1]/input")
+    @CacheLookup
+    WebElement txtUserName;
+
+    @FindBy(xpath = "/html/body/div/div/div[2]/div[1]/div/div/form/div[2]/input")
+    @CacheLookup
+    WebElement txtPassword;
+
+    @FindBy(xpath = "/html/body/div/div/div[2]/div[1]/div/div/form/input")
+    @CacheLookup
+    WebElement btnLogin;
+
+
+    public void setUserName(String uname) {
+        txtUserName.sendKeys(uname);
+    }
+
+    public void setPassword(String password) {
+        txtUserName.sendKeys(password);
+    }
+
+    public void clickLogin() {
+        btnLogin.click();
+    }
+
+
 }
--- E_Commerce_TDD_Framework/src/test/java/testCases/BaseClass.java
+++ E_Commerce_TDD_Framework/src/test/java/testCases/BaseClass.java
@@ -2,20 +2,42 @@ package testCases;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import utilities.ReadConfig;
+
+import java.util.concurrent.TimeUnit;
 
 public class BaseClass {
 
-    ReadConfig readConfig=new ReadConfig();
+    ReadConfig readconfig=new ReadConfig();
+    public String baseURL=readconfig.getApplicationURL();
     public static WebDriver driver;
     public void setUp(String browser)
     {
-        if (browser.equals("chrome"))
-        {
-            //BaseClass
-            System.setProperty("webdriver.chrome.driver",readConfig.getChromePath());
-            driver=new ChromeDriver();
+        if (browser.equals("chrome")) {
+            System.setProperty("webdriver.chrome.driver", readconfig.getChromePath());
+            driver = new ChromeDriver();
 
+        } else if (browser.equals("fireFox")) {
+            System.setProperty("webdriver.gecko.driver", readconfig.getGeckoPath());
+            driver = new FirefoxDriver();
+
+        } else if (browser.equals("ie")) {
+            System.setProperty("webdriver.ie.driver", readconfig.getMsEdgePath());
+            driver = new InternetExplorerDriver();
         }
+
+
+        driver.get(baseURL);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+
+
+    }
+
+    public void tearDown()
+    {
+        driver.quit();
     }
 }
